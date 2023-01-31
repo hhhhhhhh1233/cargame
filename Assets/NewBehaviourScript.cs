53057548efa6d9dd09ac07fdb9ad72f4f7b00e6e
hhhhhhhh <tagetjerngren@gmail.com>
2023-01-31 12:31:47 +0100

    Added a respawn button and a new perspective

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -14,23 +14,27 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-	float horizontalInput = Input.GetAxis("Horizontal");
-	float triggerInput = Input.GetAxis("Accelerate");
+		float horizontalInput = Input.GetAxis("Horizontal");
+		float triggerInput = Input.GetAxis("Accelerate");
 
 
-	transform.Rotate(0,120*horizontalInput*Time.deltaTime,0);
+		transform.Rotate(0,120*horizontalInput*Time.deltaTime,0);
 
-	//transform.RotateAround(target.transform.position, Vector3.up, 20 * Time.deltaTime);
+		//transform.RotateAround(target.transform.position, Vector3.up, 20 * Time.deltaTime);
 
-	transform.position += 10 * triggerInput * transform.forward * Time.deltaTime;
+		transform.position += 10 * triggerInput * transform.forward * Time.deltaTime;
 
-	if (Input.GetButton("Fire1"))
-	{
-		transform.position += 10 * transform.forward * Time.deltaTime;
+		if (Input.GetButton("Fire1"))
+		{
+			transform.position += 10 * transform.forward * Time.deltaTime;
+		}
+		if (Input.GetButton("Fire2"))
+		{
+			transform.position += -5 * transform.forward * Time.deltaTime;
+		}
+		if (Input.GetButton("Respawn"))
+		{
+			transform.SetPositionAndRotation(new Vector3(0,3,0), Quaternion.Euler(0, 0, 0));
+		}
 	}
-	if (Input.GetButton("Fire2"))
-	{
-		transform.position += -5 * transform.forward * Time.deltaTime;
-	}
-    }
 }
